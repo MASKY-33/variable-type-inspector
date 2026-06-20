af073f06e5a8b8ecc5fe360e3c4b6cccbbc7cea3
MASKY-33 <masky.somebody33@gmail.com>
2026-06-20 15:30:23 +0200

    Create variable-type-inspector.py

---
+++ variable-type-inspector.py
@@ -0,0 +1,12 @@
+## This Program:
+# - has 3 variables (integer, text, and boolean)
+# - prints their types with the types() function
+
+
+int_var = 33
+txt_var = "MASKY Programmer Python"
+bool_var = True
+
+print(type(int_var))
+print(type(txt_var))
+print(type(bool_var))
